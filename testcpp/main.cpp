0b087451f18d1d6e6471007c747e23a590e5cb8a
mumiao <mumiao@dtstack.com>
2022-07-21 23:09:21 +0800

    Initial Commit

---
+++ testcpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testcpp
+//
+//  Created by 郑琳 on 2022/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
